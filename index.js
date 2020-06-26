0483a3b9df3cbaf3cde749dea9404837d6bdd2ab
Howard Kennedy <hkennedyvi@gmail.com>
2020-06-25 17:57:03 -0700

    header animated

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function togglePopup() {
+togglePopup = () => {
     document.getElementById("explore-popup").classList.toggle("active");
+    document.getElementById("main-header").style.opacity = 0;
+}
+
+closeWindow = () => {
+    document.getElementById("explore-popup").classList.toggle("active");
+    document.getElementById("main-header").style.opacity = 1;
 }
